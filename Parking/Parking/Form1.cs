7551d59bebf4b0cf734c69eee54ba60110360c53
Kostandin Vllahu <33177557+kostandinvllahu@users.noreply.github.com>
2020-12-01 15:33:29 +0100

    Update Form1.cs

--- Parking/Parking/Form1.cs
+++ Parking/Parking/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Parking
 {
-    public partial class Form1 : Form
+    public partial class ParkingMeter : Form
     {
-        public Form1()
+        public ParkingMeter()
         {
             InitializeComponent();
         }
@@ -40,6 +40,7 @@ namespace Parking
             radioButton1.Checked = false;
             radioButton2.Checked = false;
             richTextBox2.Text = "";
+            Option.Text = "";
         }
 
 
@@ -54,6 +55,7 @@ namespace Parking
                 }
             if (radioButton2.Checked == true)
             {
+               
                 if (charge <= 23)
                 {
                     tekst1.Text = " ";
@@ -78,8 +80,11 @@ namespace Parking
                 }
                 sum += r.Next(0, 100);
                 richTextBox2.Text = "========PARKING TICKET========= " +
-                    "\n DATE: " + DateTime.Now.ToLongTimeString() +
-                    " \nTICKET ID. " + " AA" + sum.ToString() +
+                    "\n DATE: " + DateTime.Now.ToString("dddd , MMM dd yyyy,hh:mm:ss") +
+                " \nTICKET ID. " + " AA" + sum.ToString() +
+                    "\nOPTION: " + Option.Text +
+                    "\nHOURS: " + input1.Text +
+                    "\nPRICE: " + tekst1.Text + 
                     " \nTHANK YOU FOR USING OUR SERVICE";
                 //===============KETU TESTIM==================
                 if (radioButton2.Checked == true)
@@ -124,9 +129,16 @@ namespace Parking
         {
            if(radioButton1.Checked == true)
             {
+               
                 richTextBox1.Text = "Price for less than 24h\n" +
                     "0-3h = 2$\n" +
                     "3-23h = 2$ + 0.5$/h";
+
+                
+            }
+           if(radioButton1.Checked == true)
+            {
+                Option.Text = "Less then 24h";
             }
         }
 
@@ -138,6 +150,11 @@ namespace Parking
                     "1 day = 10$\n" +
                     "3-5 days = 35$\n" +
                     "5+ days = 35$ + 5$";
+            }
+
+            if(radioButton2.Checked == true)
+            {
+                Option.Text = "More then 24h";
             }
 
         }
@@ -170,11 +187,19 @@ namespace Parking
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("========PARKING TICKET========= " +
-                    "\n DATE: " + DateTime.Now.ToLongTimeString() +
-                    " \nTICKET ID. " + " AA" + sum.ToString() +
+                    "\n DATE: " + DateTime.Now.ToString("dddd , MMM dd yyyy,hh:mm:ss") +
+            " \nTICKET ID. " + " AA" + sum.ToString() +
+                    "\nOPTION: " + Option.Text +
+                    "\nHOURS: " + input1.Text +
+                    "\nPRICE: " + tekst1.Text +
                     " \nTHANK YOU FOR USING OUR SERVICE");
 
             Clean();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
